9f6501781c465977cc383e251f39b16fce29ec3a
Mimi <2392929637@qq.com>
2022-03-10 12:30:42 +0800

    second commit

--- src/main/java/com/blackcat/git/GitTest.java
+++ src/main/java/com/blackcat/git/GitTest.java
@@ -3,5 +3,6 @@ package com.blackcat.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,git!");
+        System.out.println("hello,git2!");
     }
 }
